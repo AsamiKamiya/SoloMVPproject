61f64209d72951eac85f73171d15fa77ca94e61b
kamiya <petit0608@gmail.com>
2020-02-11 17:20:51 +0900

    fix db setting.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: "pg",
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || "teaApp",
